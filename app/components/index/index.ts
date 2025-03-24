2a9da6b79e64d4c36d04d3661e145008f1098604
CodeEdward <108403615+edwardfox15@users.noreply.github.com>
2025-03-24 08:29:12 +0000

    commit-03

--- app/components/index/index.ts
+++ app/components/index/index.ts
@@ -1,11 +1,12 @@
+import { StaticImageData } from "next/image";
 import cloud1 from "@/public/assets/cloud1.png";
 import cloud2 from "@/public/assets/cloud2.png";
 import cloud3 from "@/public/assets/cloud3.png";
 import cloud4 from "@/public/assets/cloud4.png";
 import cloud5 from "@/public/assets/cloud5.png";
 
-export const Cl1:any = cloud1;
-export const Cl2:any = cloud2;
-export const Cl3:any = cloud3;
-export const Cl4:any = cloud4;
-export const Cl5:any = cloud5;
+export const Cl1: StaticImageData = cloud1;
+export const Cl2: StaticImageData = cloud2;
+export const Cl3: StaticImageData = cloud3;
+export const Cl4: StaticImageData = cloud4;
+export const Cl5: StaticImageData = cloud5;
